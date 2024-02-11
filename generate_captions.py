cbde77ca349dd96e70328e1d6e000984408a25fc
Haoli Yin <haoli.yin@vanderbilt.edu>
2024-02-11 13:42:02 -0600

    Finish adding captions to parquet code

--- generate_captions.py
+++ generate_captions.py
@@ -1,10 +1,14 @@
 import os
 import h5py
-import tqdm
+from tqdm import tqdm
 import tempfile
 from PIL import Image
 import time
 import concurrent.futures
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from fastparquet import write
 
 import sglang as sgl
 from sglang import set_default_backend, RuntimeEndpoint
@@ -32,50 +36,71 @@ def delete_file(file_path):
     """
     os.remove(file_path)
 
+def append_batch_to_parquet(df, parquet_file):
+    if os.path.exists(parquet_file):
+        write(parquet_file, df, append=True, compression='snappy')
+    else:
+        write(parquet_file, df, compression='snappy')
+    
+    print("Added to parquet file")
 
 if __name__ == "__main__":
     """
     We have to create temporary file paths since that's what the sglang function expects.
-    Note that I will only process 1k out of the 73k images in the dataset due to time constraints.
+    Note: that I will only process 1k out of the 73k images in the dataset due to time constraints.
     """
 
     file_path = "/home/haoli/Documents/data/nsd_images/nsd_stimuli.hdf5"
-
+    parquet_file = "captions.parquet"
     captions = []
-
+    counter = 0
     with h5py.File(file_path, 'r') as f:
-        # calculate chunk size later
-        chunk_size = 6
-        
-        data = f['imgBrick'][:chunk_size]
+ 
+        # # for 1000 total samples should take about an hour
+        for i in tqdm(range(100)):
 
-        # Parallelize the saving of images to temporary files
-        temp_file_paths = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_to_image = {executor.submit(save_image, image, i): i for i, image in enumerate(data)}
-            for future in concurrent.futures.as_completed(future_to_image):
-                temp_file_path = future.result()
-                temp_file_paths.append(temp_file_path)
+            # manually set chunk size to 10 for now
+            chunk_size = 10
+            
+            data = f['imgBrick'][counter:counter+chunk_size]
 
-        # generate captions for each image
-        for i, file_path in enumerate(temp_file_paths): 
+            # Parallelize the saving of images to temporary files
+            temp_file_paths = []
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_to_image = {executor.submit(save_image, image, i): i for i, image in enumerate(data)}
+                for future in concurrent.futures.as_completed(future_to_image):
+                    temp_file_path = future.result()
+                    temp_file_paths.append(temp_file_path)
 
-            response = image_qa.run(
-                image_path = file_path, 
-                question = "Generate a complex description of the image",
-                max_new_tokens=4096, 
-            )["answer"]
+            # generate captions for each image
+            for i, file_path in enumerate(temp_file_paths): 
 
-            captions.append(response)
+                response = image_qa.run(
+                    image_path = file_path, 
+                    question = "Generate a complex description of the image",
+                    max_new_tokens=4096, 
+                )["answer"]
 
-        # Parallelize the deletion of temporary files
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            delete_tasks = [executor.submit(delete_file, file_path) for file_path in temp_file_paths]
+                captions.append(response)
 
-            # error handling
-            for future in concurrent.futures.as_completed(delete_tasks):
-                try:
-                    future.result()
-                except Exception as e:
-                    print(f"Error deleting file: {e}")
+            # Parallelize the deletion of temporary files
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                delete_tasks = [executor.submit(delete_file, file_path) for file_path in temp_file_paths]
 
+                # error handling
+                for future in concurrent.futures.as_completed(delete_tasks):
+                    try:
+                        future.result()
+                    except Exception as e:
+                        print(f"Error deleting file: {e}")
+
+            data = {
+                'id': [str(i).zfill(5) for i in range(counter, counter + chunk_size)],
+                'caption': captions
+            }
+
+            df = pd.DataFrame(data)
+            append_batch_to_parquet(df, parquet_file)
+
+            captions = []
+            counter += chunk_size
